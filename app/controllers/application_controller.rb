35fac570190ba38a54683b1ff85f5838a1b321d7
Laura Stude <laura.stude@gmail.com>
2012-05-12 19:38:52 -0500

    fixing session timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :reset_session
+  
+  def reset_session
+    session[:player_id] = session[:player_id]
+  end
+ 
 end
